49d49385c45a73d7077101e81cd7b546bf405b23
Yaroslav Galitsyn <galicin20045@gmail.com>
2024-07-29 16:19:09 +0700

    task_2.py db connection modified

--- task_2.py
+++ task_2.py
@@ -4,38 +4,32 @@ from sqlite3 import Connection, Cursor
 from faker import Faker
 
 
-def connect_to_db() -> Tuple[Connection, Cursor]:
-    connection = sqlite3.connect("users.db")
-    cursor = connection.cursor()
-    return connection, cursor
-
-
 def create_table() -> None:
-    connection, cursor = connect_to_db()
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)"
-    )
-    connection.commit()
-    connection.close()
+    with sqlite3.connect("users.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)"
+        )
+        connection.commit()
 
 
 def insert_data() -> None:
-    connection, cursor = connect_to_db()
     fake = Faker()
-    for _ in range(100):
-        cursor.execute(
-            "INSERT INTO users (name, age) VALUES (?, ?)",
-            (fake.name(), fake.random_int(min=18, max=80))
-        )
-    connection.commit()
-    connection.close()
+    with sqlite3.connect("users.db") as connection:
+        cursor = connection.cursor()
+        for _ in range(100):
+            cursor.execute(
+                "INSERT INTO users (name, age) VALUES (?, ?)",
+                (fake.name(), fake.random_int(min=18, max=80))
+            )
+        connection.commit()
 
 
 def select_data() -> List[Tuple[str, int]]:
-    connection, cursor = connect_to_db()
-    cursor.execute("SELECT name, age FROM users WHERE age > 30")
-    users = cursor.fetchall()
-    connection.close()
+    with sqlite3.connect("users.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT name, age FROM users WHERE age > 30")
+        users = cursor.fetchall()
     return users
 
 
