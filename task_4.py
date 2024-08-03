54a95fc0ab8116865eb4558017ea9842d255e8da
Yaroslav Galitsyn <galicin20045@gmail.com>
2024-08-03 19:40:06 +0700

    task_4.py modified_script_2 added

--- task_4.py
+++ task_4.py
@@ -32,7 +32,21 @@ def modified_script() -> None:
     numbers = range(1000001)
     squares = list(map(lambda x: x ** 2, numbers))
 
+@timer
+def modified_script_2() -> None:
+    """
+    Модифицированный скрипт:
+    range создает объект-генератор, который не хранит все значения в памяти, а генерирует их по мере необходимости.
+    Аллоцируется память для списка squares, чтобы избежать повторного выделения памяти при добавлении элементов.
+    Во время итерации по объекту-генератору numbers, каждое значение возводится в квадрат и добавляется в список squares.
+    """
+    numbers = range(1000001)
+    squares = 1000001*[None]
+    for i in numbers:
+        squares[i] = i ** 2
+
 
 if __name__ == "__main__":
     base_script()
     modified_script()
+    modified_script_2()
